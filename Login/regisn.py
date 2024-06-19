d0c688a827818c356a95239c25bca748a2efe7e8
Benson <hui881556@gmail.com>
2024-06-19 14:30:33 +0800

    新增使用者登入,月卡周卡時數

---
+++ Login/regisn.py
@@ -0,0 +1,38 @@
+from werkzeug.security import generate_password_hash
+import pymysql
+
+# 假設使用者資料
+username = '120129c3'
+password = '96587423c3'
+hashed_password = generate_password_hash(password)
+
+db_host = 'localhost'
+db_user = 'root'
+db_password = '120129'
+db_name = 'card_database'
+db_port = 3300
+
+def connect_db():
+    try:
+        conn = pymysql.connect(
+            host=db_host, 
+            user=db_user, 
+            password=db_password, 
+            database=db_name, 
+            port=db_port
+        )
+        return conn
+    except pymysql.MySQLError as e:
+        print(f"Error connecting to the database: {e}")
+        return None
+    
+# 將新的使用者插入資料庫
+
+conn = connect_db()
+cursor = conn.cursor()
+
+cursor.execute('INSERT INTO users (username, password, card_number) VALUES (%s, %s, %s)', 
+               (username, hashed_password, '1234567890123456'))
+
+conn.commit()
+conn.close()
